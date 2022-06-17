11d7d28b65ebdb87c1c1e59d0f885abe4c6be884
Julien SCHMITT <jschmitt95@protonmail.com>
2022-06-17 20:52:44 +0200

    prepare for Github Actions

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [ -f ".env" ]; then
-	GITHUB_USER=$(grep GITHUB_USER .env | cut -d '=' -f2)
-	GITHUB_TOKEN=$(grep GITHUB_TOKEN .env | cut -d '=' -f2)
+	ACCOUNT_USER=$(grep ACCOUNT_USER .env | cut -d '=' -f2)
+	ACCOUNT_TOKEN=$(grep ACCOUNT_TOKEN .env | cut -d '=' -f2)
 	# else, maybe in Github secrets...
 fi
 
 # Detail about github user
-curl -i -u $GITHUB_USER:$GITHUB_TOKEN https://api.github.com/user
+curl -i -u $ACCOUNT_USER:$ACCOUNT_TOKEN https://api.github.com/user
 
 # Detail about github repo
 #curl -i -H "Authorization: token "$GITHUB_TOKEN https://api.github.com/user/repos
